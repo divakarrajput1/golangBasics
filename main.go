88231ea510f65e5c4cb65b7bf0d43064c39ba4d2
Divakar <divakar@greedygame.com>
2025-10-27 16:41:27 +0530

    added b8

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnh b6")
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb5")
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb5")
+	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb8")
+	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb8")
 }
 func main() {
 
@@ -31,7 +33,7 @@ func main() {
 
 	log.Printf("Hello %s Did you logged in?", "Divakar,")
 	//log.Fatalf("Hello something went wrong")
-	log.Println("Hello Divakar")
+	log.Println("Hello Divakar8")
 	http.HandleFunc("/hello", helloHandler)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)

48d6a64761a398b07354f0cacd65be98f0d91c04
Divakar <divakar@greedygame.com>
2025-10-27 16:18:41 +0530

    added b6

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnh b4")
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnh b4")
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnh b4")
+	fmt.Fprintf(w, "Hello, Divakar! this is bracnh b6")
+	fmt.Fprintf(w, "Hello, Divakar! this is bracnh b6")
 }
 func main() {
 

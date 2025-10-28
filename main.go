69a969fcc9371722395f45031bac8c7421d18942
Divakar <divakar@greedygame.com>
2025-10-28 11:04:34 +0530

    pointer demo hai

--- main.go
+++ main.go
@@ -33,16 +33,10 @@ func UpdateName(s *Student, newName string) {
 }
 func main() {
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	// defer cancel()
-
-	//go work(ctx)
-
-	//time.Sleep(5 * time.Second)
 	fmt.Println("Done")
 
 	log.Printf("Hello %s Did you logged in?", "Divakar,")
-	//log.Fatalf("Hello something went wrong")
+
 	log.Println("Hello Divakar")
 	log.Println("Hello Divakarb7")
 	log.Println("Hello Divakarb7")
@@ -56,16 +50,3 @@ func main() {
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
-
-// func work(ctx context.Context) {
-// 	for {
-// 		select {
-// 		case <-ctx.Done():
-// 			fmt.Println("Stop Working")
-// 			return
-// 		default:
-// 			fmt.Println("Working...")
-// 			time.Sleep(1 * time.Second)
-// 		}
-// 	}
-// }

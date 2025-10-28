2cbd98cc4fec9b6a74716f45b1de9aa441134658
Divakar <divakar@greedygame.com>
2025-10-28 11:47:06 +0530

    main.go

--- main.go
+++ main.go
@@ -23,21 +23,15 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb8")
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb8")
 }
-
-type Student struct {
-	Name    string
-	Age     int
-	Roll_no int
-}
-
-func UpdateName(s *Student, newName string) {
-	s.Name = newName
-}
 func main() {
 
-	student := &Student{Name: "Divakar", Age: 23, Roll_no: 143}
-	UpdateName(student, "Kirti")
-	fmt.Println("Student Name:", student.Name)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	go work(ctx)
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Done")
 
 	log.Printf("Hello %s Did you logged in?", "Divakar,")
 	//log.Fatalf("Hello something went wrong")
@@ -46,8 +40,20 @@ func main() {
 	log.Println("Hello Divakarb7")
 	log.Println("Hello Divakarb7")
 	log.Println("Hello Divakar8")
-
 	http.HandleFunc("/hello", helloHandler)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func work(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Stop Working")
+			return
+		default:
+			fmt.Println("Working...")
+			time.Sleep(1 * time.Second)
+		}
+	}
 }

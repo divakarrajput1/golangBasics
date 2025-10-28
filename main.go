4b85ce56dca68109f6978b743799d06c46f22fdb
Divakar <divakar@greedygame.com>
2025-10-28 10:59:27 +0530

    Pointer demo

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,14 +19,24 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb5")
 	fmt.Fprintf(w, "Hello, Divakar! this is bracnhb5")
 }
+
+type Student struct {
+	Name    string
+	Age     int
+	Roll_No int
+}
+
+func UpdateName(s *Student, newName string) {
+	s.Name = newName
+}
 func main() {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// defer cancel()
 
-	go work(ctx)
+	//go work(ctx)
 
-	time.Sleep(5 * time.Second)
+	//time.Sleep(5 * time.Second)
 	fmt.Println("Done")
 
 	log.Printf("Hello %s Did you logged in?", "Divakar,")
@@ -37,20 +45,24 @@ func main() {
 	log.Println("Hello Divakarb7")
 	log.Println("Hello Divakarb7")
 	log.Println("Hello Divakarb7")
+
+	student := &Student{Name: "Kirti", Age: 21, Roll_No: 143}
+	UpdateName(student, "Divakar")
+	fmt.Println("Student Name:", student.Name)
 	http.HandleFunc("/hello", helloHandler)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func work(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Stop Working")
-			return
-		default:
-			fmt.Println("Working...")
-			time.Sleep(1 * time.Second)
-		}
-	}
-}
+// func work(ctx context.Context) {
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			fmt.Println("Stop Working")
+// 			return
+// 		default:
+// 			fmt.Println("Working...")
+// 			time.Sleep(1 * time.Second)
+// 		}
+// 	}
+// }
